e305dd57be256a239638dbdef47ae2925fcb7faf
acer51-doctom <110639772+acer51-doctom@users.noreply.github.com>
2025-11-18 19:08:54 +0100

    bad build file (should work?)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
     namespace = "com.acer51.TheZodiacApp"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.acer51.TheZodiacApp"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 36
         versionCode = 1
         versionName = "0.4.0"
         vectorDrawables.useSupportLibrary = true
